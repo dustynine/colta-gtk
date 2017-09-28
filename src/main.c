58e50d85a4b37343472e5d0dfc21fdfd8563bd61
Dusty Nine <27400@protonmail.ch>
2017-09-28 15:15:15 +0600

    ???

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <gtk/gtk.h>
 
 static void activate(GtkApplication* app, gpointer user_data) {
-    int i;
+    int i, x, y;
+    unsigned int seed;
     const int CELLS = 40;
+    char alphabet[37] = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','x','.',',','?','!',' '};
     
     GtkWidget *window;
     GtkWidget *grid;
@@ -12,19 +15,11 @@ static void activate(GtkApplication* app, gpointer user_data) {
     GtkWidget *button;
     GtkWidget *output_grid;
     GtkWidget *output;
-
     GtkWidget *cells[CELLS];
-    //GtkWidget *color_1_1, *color_1_2, *color_1_3, *color_1_4, *color_1_5, *color_1_6,
-    //          *color_1_7, *color_1_8, *color_1_9, *color_1_10, *color_2_1, *color_2_2,
-    //          *color_2_3, *color_2_4, *color_2_5, *color_2_6, *color_2_7, *color_2_8, 
-    //          *color_2_9, *color_2_10, *color_3_1, *color_3_2, *color_3_3, *color_3_4,
-    //          *color_3_5, *color_3_6, *color_3_7, *color_3_8, *color_3_9, *color_3_10, 
-    //          *color_4_1, *color_4_2, *color_4_3, *color_4_4, *color_4_5, *color_4_6,
-    //          *color_4_7, *color_4_8, *color_4_9, *color_4_10;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Colta");
-    gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
+    //gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
 
     grid = gtk_grid_new();
     //gtk_grid_set_column_homogeneous(GTK_GRID(grid), 1);
@@ -46,14 +41,28 @@ static void activate(GtkApplication* app, gpointer user_data) {
     button = gtk_button_new_with_label("Encode");
     gtk_container_add(GTK_CONTAINER(button_box), button);
 
-    for (i=0, i<CELLS, i++) {
-        cells[i] = gtk_label_new()
+    //gtk_override_background_color(GTK_WIDGET(cells[0]), GTK_STATE_FLAG_NORMAL,
 
     output_grid = gtk_grid_new();
-    gtk_grid_attach(GTK_GRID(grid), output_grid, 0, 1, 2, 1);
-    //gtk_grid_attach(GTK_GRID(output_grid), button_box, 1, 1, 1, 1);
+    //gtk_grid_set_column_spacing(GTK_GRID(grid), 2);
+    for (i=0; i<CELLS; i++) {
+        cells[i] = gtk_label_new("██");
+    }
+
+    i=0;
+    for (y=0; y<4; y++) {
+        for (x=0; x<10; x++) {
+            gtk_grid_attach(GTK_GRID(output_grid), cells[i], x, y, 1, 1);
+            i++;
+        }
+    }
+
+    gtk_grid_attach(GTK_GRID(grid), output_grid, 0, 2, 2, 1);
 
     gtk_widget_show_all(window);
+}
+
+void encode() {
 }
 
 
@@ -71,3 +80,4 @@ int main(int argc, char **argv) {
 }
 
 // 255 characters = 4 rows 10 columns of colors
+// seed between 0 to 4'294'967'295
