f1374c6f0bcb76ae4bbd1d54d55bc18c59ec6373
Dusty Nine <27400@protonmail.ch>
2017-11-10 00:58:29 +0600

    rewrote with struct

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-gcc `pkg-config --cflags gtk+-3.0` -o colta-gtk src/main.c `pkg-config --libs gtk+-3.0`
+gcc `pkg-config --cflags gtk+-3.0` -o colta-gtk src/main.c -g `pkg-config --libs gtk+-3.0`
--- src/main.c
+++ src/main.c
@@ -8,25 +8,38 @@
 #define TWO_TENS 256
 #define FOUR_TENS 65536
 
-const gchar ALPHABET[LETTERS] = 
+const gchar ALPHABET[LETTERS] =
     {'a','b','c','d','e','f','g','h','i','j','k', 'l','m', 'n','o','p','q','r',
      's','t','u','v','w','x','y','z','.',',','?','!',' '};
 const int DEFAULT_CODE[LETTERS] =
     {7,14,21,28,35,42,49,56,63,70,77,84,91,98,105,112,119,
      126,133,140,147,154,161,168,175,182,189,196,203,210,217};
 
+struct template {
+    GtkWidget *window;
+    GtkWidget *grid;
+    GtkWidget *plaintext;
+    GtkWidget *key;
+    GtkWidget *button_box;
+    GtkWidget *button;
+    GtkWidget *output_grid;
+    GtkWidget *cells[CELLS];
+};
+
 struct passed_widgets {
     GtkWidget *plaintext;
     GtkWidget *key;
+    GtkWidget **cls;
 };
 
 
-gpointer make_data(GtkWidget *text, GtkWidget *key) {
-    static struct passed_widgets pw;
-    pw.plaintext = text;
-    pw.key = key;
-    return (gpointer) &pw;
-}
+//gpointer make_data(GtkWidget *text, GtkWidget *key, GtkWidget *cells[CELLS]) {
+//    static struct passed_widgets pw;
+//    pw.plaintext = text;
+//    pw.key = key;
+//    pw.cls = cells;
+//    return &pw;
+//}
 
 
 int *make_triplets(gchar str[]) {
@@ -38,7 +51,6 @@ int *make_triplets(gchar str[]) {
 
     for (i = 0; str[i] != '\0'; i++) {
         for (j = 0; str[i] != ALPHABET[j]; j++);
-            //printf("        str[%i] = %c; ALPHABET[%i] = %c\n", i, str[i], j, ALPHABET[j]);
         printf("        DEFAULT_CODE[%i] = 0x%x\n", j, DEFAULT_CODE[j]);
         temp[i] = DEFAULT_CODE[j];
         printf("    temp[%i] = %i\n", i, temp[i]);
@@ -52,14 +64,10 @@ int *make_triplets(gchar str[]) {
         temp[i] = DEFAULT_CODE[30];
     }
 
-    // сделать счетчик количества символов, делать что-то если кол-во не равно трем
-
     for (start = 0; start < i; start+=3) {
         triplets[k] = temp[start]*FOUR_TENS + temp[start+1]*TWO_TENS + temp[start+2];
-        //printf("    1/ triplets[%i] = %x\n", k, triplets[k]);
         k++;
     }
-    // умножать на 256 и складывать по разрядам
     return triplets;
 }
 
@@ -67,128 +75,114 @@ int *make_triplets(gchar str[]) {
 gchar *strip_string(gchar str[]) {
     int i, j, k = 0;
     static gchar cleantext[CAPACITY];
-    //printf("      iterating through the raw string\n");
     for (i = 0; str[i] != '\0'; i++) {
-        //printf("         str[%i] = %c\n", i, str[i]);
         for (j = 0; str[i] != ALPHABET[j] && j < LETTERS; j++);
-        //printf("         j=%i;\n", j);
         if (j == LETTERS) {
-            //printf("      dropping unknown character\n");
             continue;
         }
         else {
-            //printf("      match found: %c\n", ALPHABET[j]);
             cleantext[k] = ALPHABET[j];
             k++;
         }
     }
     cleantext[k] = '\0';
-    //printf("     cleantext is:%s\n", cleantext);
     return cleantext;
 }
 
 
-static void encode(GtkButton *button, gpointer *data) {
+static void encode(GtkButton *button, gpointer data) {
     printf("encoding initiated:\n");
 
     int k = 0;
     int *triplets;
     gchar *plaintext;
     gchar *key;
-    GtkTextIter start, end;
-    GtkTextBuffer *txt_buf;
-    GtkEntryBuffer *ent_buf;
-    struct passed_widgets *pw;
-    
-    pw = (struct passed_widgets *) data;
-    
-    txt_buf = gtk_text_view_get_buffer(GTK_TEXT_VIEW(pw -> plaintext));
-    gtk_text_buffer_get_bounds(txt_buf, &start, &end);
-    plaintext = gtk_text_buffer_get_text(txt_buf, &start, &end, FALSE);
-    printf("   plaintext contents: %s\n", plaintext);
-    
-    ent_buf = gtk_entry_get_buffer(GTK_ENTRY(pw -> key));
-    key = gtk_entry_buffer_get_text(ent_buf);
-    printf("   key contents: %s\n", key);
+    struct template *pw;
 
-    triplets = make_triplets(strip_string(plaintext));
-    for (int i = 0; i<4; i++) {
-        for (int j = 0; j<10; j++) {
-            printf("[%02i]:%06x ", k, triplets[k]);
-            k++;
-        }
-        printf("\n");
-    }
+    pw = (struct template *) data;
+    
+    printf("data address: %p\n", (struct template *)data);
+    printf("pw address: %p\n", pw);
+    printf("*pw address: %p\n", *pw);
+    printf("pw -> plaintext address: %p\n", pw -> plaintext);
+    printf("pw.plaintext address: %p", *(pw) -> plaintext);
+    //plaintext = gtk_entry_get_text(GTK_ENTRY(pw -> plaintext));
+    //printf("   plaintext contents: %s\n", plaintext);
+
+    //key = gtk_entry_buffer_get_text(gtk_entry_get_buffer(GTK_ENTRY(&(pw -> key))));
+    //gtk_entry_set_buffer(GTK_ENTRY(pw -> key),gtk_entry_get_buffer(GTK_ENTRY(pw -> plaintext)));
+    //printf("   key contents: %s\n", key);
+
+    //triplets = make_triplets(strip_string(plaintext));
+    //for (int i = 0; i<4; i++) {
+    //    for (int j = 0; j<10; j++) {
+    //        printf("[%02i]:%06x ", k, triplets[k]);
+    //        k++;
+    //    }
+    //    printf("\n");
+    //}
+
+    //GdkColor color;
+    //gdk_color_parse("#07d9d9", &color);
+    //gtk_widget_modify_fg(GTK_WIDGET(pw -> cells), GTK_STATE_NORMAL, &color);
 }
 
 
 static void activate(GtkApplication* app, gpointer user_data) {
     int i, x, y;
     unsigned int seed;
+    struct template gui;
 
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *plaintext;
-    GtkWidget *key;
-    GtkWidget *button_box;
-    GtkWidget *button;
-    GtkWidget *output_grid;
-    GtkWidget *output;
-    GtkWidget *cells[CELLS];
+    gui.window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(gui.window), "Colta");
 
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Colta");
-    //gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
+    gui.grid = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(gui.grid), 3);
+    gtk_grid_set_column_spacing(GTK_GRID(gui.grid), 3);
+    gtk_container_add(GTK_CONTAINER(gui.window), gui.grid);
 
-    grid = gtk_grid_new();
-    //gtk_grid_set_column_homogeneous(GTK_GRID(grid), 1);
-    //gtk_grid_set_row_homogeneous(GTK_GRID(grid), 1);
-    gtk_grid_set_row_spacing(GTK_GRID(grid), 3);
-    gtk_grid_set_column_spacing(GTK_GRID(grid), 3);
-    gtk_container_add(GTK_CONTAINER(window), grid);
+    gui.plaintext = gtk_entry_new();
+    gtk_grid_attach(GTK_GRID(gui.grid), gui.plaintext, 0, 0, 2, 1);
+    printf("gui.plaintext address: %p\n", gui.plaintext);
 
-    plaintext = gtk_text_view_new();
-    gtk_text_view_set_editable(GTK_TEXT_VIEW(plaintext), 1);
-    gtk_text_view_set_accepts_tab(GTK_TEXT_VIEW(plaintext), 0);
-    gtk_grid_attach(GTK_GRID(grid), plaintext, 0, 0, 2, 1);
+    gui.key = gtk_entry_new();
+    gtk_grid_attach(GTK_GRID(gui.grid), gui.key, 0, 1, 1, 1);
 
-    key = gtk_entry_new();
-    gtk_grid_attach(GTK_GRID(grid), key, 0, 1, 1, 1);
+    gui.button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+    gtk_grid_attach(GTK_GRID(gui.grid), gui.button_box, 1, 1, 1, 1);
 
-    button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-    gtk_grid_attach(GTK_GRID(grid), button_box, 1, 1, 1, 1);
+    gui.button = gtk_button_new_with_label("Encode");
+    gtk_container_add(GTK_CONTAINER(gui.button_box), gui.button);
 
-    button = gtk_button_new_with_label("Encode");
-    gtk_container_add(GTK_CONTAINER(button_box), button);
-
-    g_signal_connect(
-            G_OBJECT(button),
-            "clicked",
-            G_CALLBACK(encode),
-            make_data(plaintext, key)
-    );
-
-    //gtk_override_background_color(GTK_WIDGET(cells[0]), GTK_STATE_FLAG_NORMAL,
-
-    output_grid = gtk_grid_new();
-    gtk_grid_set_column_spacing(GTK_GRID(grid), 2);
-    gtk_grid_set_column_homogeneous(GTK_GRID(output_grid), 1);
-    gtk_grid_set_row_homogeneous(GTK_GRID(output_grid), 1);
+    gui.output_grid = gtk_grid_new();
+    gtk_grid_set_column_spacing(GTK_GRID(gui.grid), 2);
+    gtk_grid_set_column_homogeneous(GTK_GRID(gui.output_grid), 1);
+    gtk_grid_set_row_homogeneous(GTK_GRID(gui.output_grid), 1);
     for (i=0; i<CELLS; i++) {
-        cells[i] = gtk_label_new("██");
+        gui.cells[i] = gtk_label_new("██");
     }
 
     i=0;
     for (y=0; y<4; y++) {
         for (x=0; x<10; x++) {
-            gtk_grid_attach(GTK_GRID(output_grid), cells[i], x, y, 1, 1);
+            gtk_grid_attach(GTK_GRID(gui.output_grid), gui.cells[i], x, y, 1, 1);
             i++;
         }
     }
 
-    gtk_grid_attach(GTK_GRID(grid), output_grid, 0, 2, 2, 1);
+    gtk_grid_attach(GTK_GRID(gui.grid), gui.output_grid, 0, 2, 2, 1);
 
-    gtk_widget_show_all(window);
+    g_signal_connect(
+            G_OBJECT(gui.button),
+            "clicked",
+            G_CALLBACK(encode),
+            &gui
+            //make_data(gui.plaintext, gui.key, gui.cells)
+    );
+
+    printf("gui address: %p\n", gui);
+    printf("&gui address: %p\n", &gui);
+    gtk_widget_show_all(gui.window);
 }
 
 
